ac81395c90ed15046c4dde0141888d9aca2da370
KonradSzafer <szafer.konrad@gmail.com>
2022-06-17 20:44:17 +0200

    added conversion script

--- torch/convert_to_script.sh
+++ torch/convert_to_script.sh
@@ -1,2 +1,30 @@
 #!/usr/bin/env bash
+
+# convert to raw python script
 jupyter nbconvert --to script country_classification_torch.ipynb
+
+# add tabs after script beginning
+FILENAME="country_classification_torch.py"
+
+readarray -t file_lines < $FILENAME
+truncate -s 0 $FILENAME
+add_indent=False
+script=()
+
+for line in "${file_lines[@]}"; do
+    if [ "$add_indent" == True ]; then
+        line="    $line"
+        script+=("$line")
+    else
+        script+=("$line")
+    fi
+    if [ "$line" == "# !script" ]; then
+        # adding indent from now
+        script+=("if __name__ == '__main__':")
+        add_indent=True
+    fi
+done
+
+for line in "${script[@]}"; do
+    echo "$line" >> "$FILENAME"
+done
---
+++ torch/run_training_script.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+conda activate deep_geo_guessr
+python3 country_classification_torch.py
